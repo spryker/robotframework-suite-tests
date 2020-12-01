36a76703c791be3b97b59486fe03128cedcb8c9c
Anastasiia Chistokhvalova <anastasiiachistokhvalova@Anastasiias-MBP.fritz.box>
2020-12-01 17:22:38 +0100

    cleaned json files from unnecessary environments

--- Resources/Libraries/common.py
+++ Resources/Libraries/common.py
@@ -6,14 +6,17 @@ def defineEnvironmentVariablesFromJsonFile(env):
     print(f'Looking for {env} environment')
     variables = {}
     environments_file_path = 'Resources/Environments/environments_' + env + '.json'
-    with open(environments_file_path) as json_file:      
-        data = json.loads(json_file.read())
-        for d in data:
-            if d["environment"] == env:
-                print(f'Found an environment {env}!')
-                for key, value in d.items():
-                    if key not in variables:
-                        variables[key] = value
-    if not variables:
+    try:
+        with open(environments_file_path) as json_file:      
+            data = json.loads(json_file.read())
+            for d in data:
+                if d["environment"] == env:
+                    print(f'Found an environment {env}!')
+                    for key, value in d.items():
+                        if key not in variables:
+                            variables[key] = value
+    except:
         print(f'Environment {env} not found!')
+    if not variables:
+        print(f'No variables found for environment {env}')
     return variables
