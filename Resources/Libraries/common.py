e606c1f413bc12d9059d54587189a4aa12a9d973
anastasia-chistokhvalova <anastasia.chistokhvalova@spryker.com>
2021-07-07 19:18:06 +0200

    added a custom keyword to put miltiline table-like arguments into dictionaries

--- Resources/Libraries/common.py
+++ Resources/Libraries/common.py
@@ -1,6 +1,7 @@
 """A library for custom keywords
 """
 import json
+import re
 
 def defineEnvironmentVariablesFromJsonFile(env):
     print(f'Looking for {env} environment')
@@ -30,3 +31,24 @@ def get_url_without_starting_slash(string):
         return string_without_starting_slash
     else:
         return string
+
+def fill_variables_from_text_string(*args):
+	# the multi-line variable in a table form is splitted, keys are the 0 element and values are the 1st element. 
+	#Both are just strings with | separators
+    argums = ' '.join(args)
+    keys = (argums.split(' || '))[0]
+    values = (argums.split(' || '))[1]   
+    # now the strings are splitted by "|" separator. Also strip() removes all whitespaces
+    keys_list = (keys.split('|'))
+    values_list = (values.split('|'))
+    dictionatry = dict()
+    for item in keys_list:
+        # 'if item' checks if the key value is not empty and iterates only over keys that have a value
+        if item:
+    	    # interate through all keys, get their indexes and assign to each a value with the same index and put them into disctionary
+            # strip() removed all whitespaces
+            key = item.strip()
+            index = keys_list.index(item)
+            value = values_list[index].strip()
+            dictionatry[key] = value
+    return dictionatry
